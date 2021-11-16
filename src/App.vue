63e6a480965942329654c6084f4fc453a3cfe4e6
Jemshit Shukurov <jemshit.shukurov@oneitsol.com>
2021-11-16 18:06:41 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -21,8 +19,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+}
+* {
+  margin: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="header">
+        <div>
+            <img src="@/assets/logo.png" alt="Logo" >
+            Text
+        </div>
+        <div class="right-corner">
+            <div>
+                View Website
+            </div> 
+            <div>
+                name of user
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.header , .header div{
+    background: #272727;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: blanchedalmond;
+}
+.header img  {
+    margin: 0 20px;
+    width: 80px;
+}
+.right-corner div{
+    margin-right: 20px;
+}
+</style>
